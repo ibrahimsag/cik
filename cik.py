5d0693e6a570ef7ec377a03cdb927f321b121869
Kamyar Ghasemlou <k.ghasemlou@gmail.com>
2014-12-06 18:50:42 +0200

    added conf.cfg file && implemented init, status, reset functions in clk.py

--- cik.py
+++ cik.py
@@ -3,23 +3,59 @@
 """cik.
 USAGE:
 	cik.py reset
-	cik.py init ([-]| --addr=<wallet_address>)
+	cik.py init ([-]| --addr=<wallet_address> --pwd=<wallet_password>)
 	cik.py status
 	cik.py (-h | --help)
 	cik.py --version
 Options:
-  -h --help     Show this screen.
-  --version     Show version.
+  -h --help	  Show this screen.
+  --version	  Show version.
 """
 from docopt import docopt
+import ConfigParser
+import getpass
+W_ADDR = ""
+W_PWD = ""
+
+def init(addr, pwd, config):
+	config.set("user_info", "wallet.address", addr)
+	config.set("user_info", "wallet.password", pwd)
+	with  open("conf.cfg",'w') as cfgfile:
+		config.write(cfgfile)
+		print "Update successful"
+	return
+def printStatus():
+	if not W_ADDR or not W_PWD:
+		print "Address/Password is not set, please set these values by running:"
+		print"\tcik.py init --addr=<wallet_address> --pwd=<wallet_password>"
+		print "\tor"
+		print"\tcik.py init -"
+	return
+def reset():
+	config.set("user_info", "wallet.address", "")
+	config.set("user_info", "wallet.password", "")
+	with  open("conf.cfg",'w') as cfgfile:
+		config.write(cfgfile)
+		print "Reset successful"
+	return
+
+
 
 if __name__ == '__main__':
 	#TODO Read cnf file
-    arguments = docopt(__doc__)
-    print(arguments)
-    if arguments['init']:
-    	init(arguments['--addr'])
-    elif arguments['status']:
-    	printStatus()
-    elif arguments['reset']:
-    	reset()
+	arguments = docopt(__doc__)
+	config = ConfigParser.ConfigParser()
+	config.read('conf.cfg')
+	W_ADDR = config.get('user_info', 'wallet.address')
+	W_PWD = config.get('user_info', 'wallet.password')
+	print(arguments)
+	if arguments['init']:
+		if arguments['--addr'] and arguments['--pwd']:
+			init(arguments['--addr'], arguments['--pwd'], config)
+		else:
+			init(raw_input("Your wallet address:"), getpass.getpass("Your wallet password:"), config)
+
+	elif arguments['status']:
+		printStatus()
+	elif arguments['reset']:
+		reset()
