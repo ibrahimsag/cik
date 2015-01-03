f5483f76528fbc08a7bd9901ba0fcd256a91ea54
Kamyar Ghasemlou <k.ghasemlou@gmail.com>
2015-01-03 17:31:39 +0200

    added function descriptions

--- cik.py
+++ cik.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python
 """cik.
 USAGE:
-	cik.py reset
+	cik.py send ([-]| --recv=<receiver_wallet_address> --amount=<amount_to_be_sent>)
 	cik.py init ([-]| --addr=<wallet_address> --pwd=<wallet_password>)
+	cik.py reset
 	cik.py status
 	cik.py (-h | --help)
 	cik.py --version
@@ -18,23 +19,29 @@ W_ADDR = ""
 W_PWD = ""
 
 def init(addr, pwd, config):
+	"Set addr and pwd as wallet address and wallet secret"
 	config.set("user_info", "wallet.address", addr)
 	config.set("user_info", "wallet.password", pwd)
 	with  open("conf.cfg",'w') as cfgfile:
 		config.write(cfgfile)
 		print "Update successful"
 	return
+
 def printStatus():
+	"Get wallet address and baalance"
 	if not W_ADDR or not W_PWD:
 		print "Address/Password is not set, please set these values by running:"
 		print"\tcik.py init --addr=<wallet_address> --pwd=<wallet_password>"
 		print "\tor"
 		print"\tcik.py init -"
 	else:
+		#TODO: query [module] to get current balance
 		print "Address:\t%s"%W_ADDR
 
 	return
+
 def reset():
+	"Clear Account info from local config file"
 	config.set("user_info", "wallet.address", "")
 	config.set("user_info", "wallet.password", "")
 	with  open("conf.cfg",'w') as cfgfile:
@@ -42,6 +49,11 @@ def reset():
 		print "Reset successful"
 	return
 
+def send(recv, amount):
+	"Sends amount to recv(addr) from local wallet address"
+	#TODO: call [module] to construct, sign and send transaction message
+	return
+#TODO add option to generate  brand-new addr/pwd
 
 
 if __name__ == '__main__':
