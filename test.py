8d667fa866a6ff42a19908cf19fab60d4413b436
ibrahim <ibrahim@udemy.com>
2015-01-03 22:28:26 +0200

    testing chain.com

--- test.py
+++ test.py
@@ -7,14 +7,17 @@ from pycoin.tx.tx_utils import create_tx, sign_tx, create_signed_tx
 from pycoin.serialize import h2b
 from pycoin import encoding
 
-NETWORK = 'BTCTEST'
+NETWORK = 'testnet3'
 
-API_URL = 'https://chain.so/api/v2/'
+API_URL = 'api.chain.com/v2/'
+
+API_KEY = "146632b8b1ad98d188f41f139eb1299d"
+API_KEY_SECRET = "c41d219707d1aba2909084223ac2fd37"
 
 def make_request_url(method, *params):
-    url_format = API_URL + '{}/{}/{}'
+    url_format = 'https://{}:{}@' + API_URL + '{}/{}/{}'
     param_string = '/'.join(params)
-    url_string = url_format.format(method, NETWORK, param_string)
+    url_string = url_format.format(API_KEY, API_KEY_SECRET, NETWORK, method, param_string)
     return url_string
 
 def spendables_for_address(bitcoin_address):
@@ -22,14 +25,14 @@ def spendables_for_address(bitcoin_address):
     Return a list of Spendable objects for the
     given bitcoin address.
     """
-    response = requests.get(make_request_url('get_tx_unspent', bitcoin_address))
+    response = requests.get(make_request_url('addresses', bitcoin_address, 'unspents'))
     r = response.json()
     spendables = []
-    for u in r.get("data", {}).get("txs", []):
-        coin_value = btc_to_satoshi(u.get("value"))
+    for u in r:
+        previous_hash = h2b(u.get("transaction_hash"))
+        previous_index = u.get("output_index")
+        coin_value = u.get("value")
         script = h2b(u.get("script_hex"))
-        previous_hash = h2b(u.get("txid"))
-        previous_index = u.get("output_no")
         spendables.append(Spendable(coin_value, script, previous_hash, previous_index))
     return spendables
 
@@ -51,12 +54,13 @@ class Wallet:
 wallet1 = Wallet('wallet1.json')
 wallet2 = Wallet('wallet2.json')
 
-response = requests.get(make_request_url('get_address_balance', wallet1.address))
+url = make_request_url('addresses', wallet1.address)
+response = requests.get(url)
+print(response.content)
 
-print(response.json())
-response = requests.get(make_request_url('get_address_balance', wallet2.address))
-
-print(response.json())
+url = make_request_url('addresses', wallet2.address)
+response = requests.get(url)
+print(response.content)
 
 spendables = spendables_for_address(wallet1.address)
 for i in spendables:
@@ -67,11 +71,14 @@ for i in spendables:
 # sign_tx(tx, wifs=[wallet1.wif], secret_exponent_db=wallet1.secret_exponent_db)
 
 tx = create_signed_tx(spendables=spendables,
-               payables=[(wallet2.address, btc_to_satoshi(1))],
+               payables=[(wallet2.address, 10000)],
                wifs=[wallet1.wif], secret_exponent_db=wallet1.secret_exponent_db,
                fee=0)
-tx_payload = {'tx_hex': tx.id()}
+tx_payload = {'signed_hex': tx.id()}
+print(tx.id())
 headers = {'content-type':'application/json'}
-response = requests.post(make_request_url('send_tx'), data=json.dumps(tx_payload), headers=headers)
+url = make_request_url('transactions', 'send')
+print(url)
+response = requests.post(url, data=json.dumps(tx_payload), headers=headers)
 print(response.json())
 
