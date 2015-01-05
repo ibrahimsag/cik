12c24a8e09aab06b378d331957518ddbe221937c
Kamyar Ghasemlou <k.ghasemlou@gmail.com>
2015-01-05 17:58:58 +0200

    send implemented

--- cik.py
+++ cik.py
@@ -4,7 +4,7 @@ USAGE:
 	cik.py send ([-]| --recv=<receiver_wallet_address> --amount=<amount_to_be_sent>)
 	cik.py init ( --new | [-] | --addr=<wallet_address> --pwd=<wallet_password>)
 	cik.py reset
-	cik.py status
+	cik.py status ( --addr=<wallet_address | [-] )
 	cik.py (-h | --help)
 	cik.py --version
 Options:
@@ -21,32 +21,36 @@ import json
 
 W_ADDR = ""
 W_PWD = ""
+W_WIF_UNCOMP = ""
 
 def init(addr, pwd, config):
 	"Set addr and pwd as wallet address and wallet secret"
 	config.set("user_info", "wallet.address", addr)
 	config.set("user_info", "wallet.password", pwd)
-	wif = subprocess.Popen("ku -W " + pwd, shell=True, stdout=subprocess.PIPE).stdout.read()
+	wif = subprocess.Popen("ku -uW " + pwd, shell=True, stdout=subprocess.PIPE).stdout.read()
 	config.set("user_info", "wallet.wif", wif)
 	with open("conf.cfg",'w') as cfgfile:
 		config.write(cfgfile)
 		print "Config file update successful"
 	return
 
-def printStatus():
+def printStatus(addr=""):
 	"Get wallet address and baalance"
-	if not W_ADDR or not W_PWD:
-		print "Address/Password is not set, please set these values by running:"
-		print"\tcik.py init --addr=<wallet_address> --pwd=<wallet_password>"
-		print "\tor"
-		print"\tcik.py init -"
-	else:
-		#TODO: query [module] to get current balance
-		print "Address:\t%s"%W_ADDR
-		confirmed_dic,total_dic = pychain.getBalance(W_ADDR)
-		pending = total_dic['balance'] - confirmed_dic['balance']
-		absolute = confirmed_dic['balance']
-		print "Balance: {0} \t Pending: {1}".format(absolute, pending)
+	if not addr:
+		if not W_ADDR:
+			print "Address/Password is not set, please set these values by running:"
+			print"\tcik.py init --addr=<wallet_address> --pwd=<wallet_password>"
+			print "\tor"
+			print"\tcik.py init -"
+			return
+		else:
+			addr = W_ADDR
+	#TODO: query [module] to get current balance
+	print "Address:\t%s"%addr
+	confirmed_dic,total_dic = pychain.getBalance(addr)
+	pending = total_dic['balance'] - confirmed_dic['balance']
+	absolute = confirmed_dic['balance']
+	print "Balance: {0} \t Pending: {1}".format(absolute, pending)
 
 	return
 
@@ -61,7 +65,10 @@ def reset():
 
 def send(recv, amount):
 	"Sends amount to recv(addr) from local wallet address"
+	res = subprocess.Popen("node ./nodeapp/app.js %s %s %s %d"%(W_ADDR, W_WIF_UNCOMP, recv, int(amount)), shell=True, stdout=subprocess.PIPE).stdout.read()
 	#TODO: call [module] to construct, sign and send transaction message
+	print "node ./nodeapp/app.js %s %s %s %d"%(W_ADDR, W_WIF_UNCOMP, recv, int(amount))
+	print res
 	return
 
 def generate():
@@ -78,6 +85,7 @@ if __name__ == '__main__':
 	config.read('conf.cfg')
 	W_ADDR = config.get('user_info', 'wallet.address')
 	W_PWD = config.get('user_info', 'wallet.password')
+	W_WIF_UNCOMP = config.get('user_info', 'wallet.wif')
 	print(arguments)
 	if arguments['init']:
 		if arguments['--new']:
@@ -86,9 +94,16 @@ if __name__ == '__main__':
 			init(arguments['--addr'], arguments['--pwd'], config)
 		else:
 			init(raw_input("Your wallet address:"), getpass.getpass("Your wallet password:"), config)
-
+	elif arguments['send']:
+		if arguments['--recv'] and arguments['--amount']:
+			send(arguments['--recv'],arguments['--amount'])
+		else:
+			send(raw_input(" Please specify the receiver:\t"), raw_input("How much do you want to send:\t"))
 	elif arguments['status']:
-		printStatus()
+		if arguments["--addr"]:
+			printStatus(arguments["--addr"])
+		else:
+			printStatus()
 	elif arguments['reset']:
 		reset()
 	elif arguments['--version']:
